1ceb054f43375e0eaec1db1c8347cb6aa74cc58d
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-14 06:01:10 +0400

    creating intro page component

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,41 @@
+import "./Intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
+
+export default function Intro() {
+  const textRef = useRef(null)
+
+  useEffect(() => {
+    if (textRef.current) {
+      if (!textRef.current.dataset.initialized) {
+        init(textRef.current, {
+          showCursor: true,
+          backDelay: 1500,
+          backSpeed: 60,
+          strings: ["დაიწყე სწავლა", "მიიღე სერთიფიკატი", "გამოიყენე პრაქტიკაში"]
+        });
+        textRef.current.dataset.initialized = "true";
+      }
+    }
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="container">
+          <h1>მოძებნე შენთვის სასურველი კურსი</h1>
+          <h2>შენთვის სასურველ პროფესიაში და შემდეგ</h2>
+          <h3><span ref={textRef}></span></h3>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <img src="assets/intro/4.png" alt="" />
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down arrow.png" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+}
