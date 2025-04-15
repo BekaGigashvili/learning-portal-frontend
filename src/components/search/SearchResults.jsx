cf1c65b32835345a8369bc3b72b5519d10412972
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-16 03:54:03 +0400

    changing page behaviour

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -2,12 +2,33 @@ import React, { useEffect, useState } from "react";
 import "./SearchResults.scss"; // optional, for styles
 import axios from "axios";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SearchResults = () => {
   const [courses, setCourses] = useState([]);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const searchTerm = queryParams.get("q");
+  const handleMouseMove = (e, index) => {
+    const card = document.getElementById(`card-${index}`);
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    card.style.boxShadow = `${x * 0.05}px ${y * 0.05}px 20px #970747`;
+  };
+
+  const handleMouseLeave = (index) => {
+    const card = document.getElementById(`card-${index}`);
+    card.style.boxShadow = `0 2px 6px rgba(0, 0, 0, 0.1)`; // reset
+  };
+
+  const navigate = useNavigate();
+
+  const handleCardClick = (id) => {
+    navigate(`/courses/${id}`);
+  };
+
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -28,11 +49,18 @@ const SearchResults = () => {
 
   return (
     <div className="search-results">
-      <h2>Results for "{searchTerm}"</h2>
       <div className="card-grid">
         {courses.map((course, index) => (
-          <div key={index} className="card">
+          <div
+            key={index}
+            id={`card-${index}`}
+            className="card"
+            onClick={() => handleCardClick(course.id)}
+            onMouseMove={(e) => handleMouseMove(e, index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+          >
             <img src={course.thumbnailURL} alt={course.name} />
+            <div className="overlay">ნახე სილაბუსი</div>
             <div className="card-content">
               <h3>{course.name}</h3>
               <p>{course.description}</p>
