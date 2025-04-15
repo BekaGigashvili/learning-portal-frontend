b34987537fe2bd1419ad52521765fefeed1b4eab
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-15 04:04:27 +0400

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Topbar from './components/topbar/Topbar.jsx'
 import Menu from './components/menu/Menu.jsx'
-
+import Login from './components/login/Login.jsx'
 import { useState } from "react";
 import Intro from './components/intro/Intro.jsx';
+import Register from './components/registration/Register.jsx';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+import SearchResults from './components/search/SearchResults.jsx'
 
 const NotFound = () => <h1>404 - Page Not Found</h1>;
 
@@ -18,6 +21,10 @@ const App = () => {
       <div className='content'>
         <Routes>
           <Route path="/" element={<Intro />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/search" element={<SearchResults />} />
           
           <Route path="*" element={<NotFound />} />  {/* Catch-all for 404 */}
         </Routes>
