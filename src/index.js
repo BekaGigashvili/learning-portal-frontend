84f31a87455205c4519879eb1f38f9fb724a96e0
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-16 19:49:59 +0400

    automatic logout when token expires

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +9,25 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      const message = error.response.data;
+
+      if (typeof message === "string" && message.toLowerCase().includes("expired")) {
+
+        alert("Session expired. Please log in again.");
+      } else {
+        alert("You are not authorized.");
+      }
+
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
